618e7d132eb996ac451d4783786b496009081518
OLIVIER\olivi <info@webinord.ca>
2023-10-26 15:37:51 -0400

    5e push

--- resources/views/admin/partials/adminnav.blade.php
+++ resources/views/admin/partials/adminnav.blade.php
@@ -19,16 +19,10 @@
             </li>
             @endcan
             <li class="navLink">
-                <a href="/admin/posts"><i class="fa fa-hand-middle-finger mr-4 mt-0.5" aria-hidden="true"></i>Juifs</a>
-            </li>
-            <li class="navLink">
                 <a href="/admin/posts"><i class="fa fa-newspaper mr-4 mt-0.5" aria-hidden="true"></i>Posts</a>
             </li>
             <li class="navLink">
                 <a href="/admin/events"><i class="fa-solid fa-bell mr-4 mt-0.5" aria-hidden="true"></i>Events</a>
-            </li>
-            <li class="navLink">
-                <a href="/admin/events"><i class="fa-solid fa-bell mr-4 mt-0.5" aria-hidden="true"></i>Route oli2</a>
             </li>
             <li class="navLink">
                 <a href="{{ route('medias.index') }}"><i class="fa fa-download mr-4 mt-0.5" aria-hidden="true"></i>Médias</a>
