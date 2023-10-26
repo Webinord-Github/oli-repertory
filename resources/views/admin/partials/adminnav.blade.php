d64be9a98ede16ad84b0d2fa38c6603734f12774
OLIVIER\olivi <info@webinord.ca>
2023-10-26 15:31:26 -0400

    troisieme push oli

--- resources/views/admin/partials/adminnav.blade.php
+++ resources/views/admin/partials/adminnav.blade.php
@@ -28,7 +28,7 @@
                 <a href="/admin/events"><i class="fa-solid fa-bell mr-4 mt-0.5" aria-hidden="true"></i>Events</a>
             </li>
             <li class="navLink">
-                <a href="/admin/events"><i class="fa-solid fa-bell mr-4 mt-0.5" aria-hidden="true"></i>Route oli</a>
+                <a href="/admin/events"><i class="fa-solid fa-bell mr-4 mt-0.5" aria-hidden="true"></i>Route</a>
             </li>
             <li class="navLink">
                 <a href="{{ route('medias.index') }}"><i class="fa fa-download mr-4 mt-0.5" aria-hidden="true"></i>Médias</a>
